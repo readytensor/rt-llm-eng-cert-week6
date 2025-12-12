9b68621ccff6c3860b972440755dc940651cc6ab
rt-desai <abhyuday.desai@readytensor.com>
2025-12-11 22:54:51 -0800

    update paths

--- code/paths.py
+++ code/paths.py
@@ -9,8 +9,8 @@ if IS_SAGEMAKER:
     CODE_DIR = "/opt/ml/code"
     CONFIG_FILE_PATH = "/opt/ml/code/config.yaml"
     DATA_DIR = "/opt/ml/input/data"
-    DATASETS_DIR = "/tmp/datasets"  # Use tmp for caching
-    OUTPUTS_DIR = "/opt/ml/model"  # <-- SageMaker uploads this to S3 automatically
+    DATASETS_DIR = "/tmp/datasets"
+    OUTPUTS_DIR = "/opt/ml/model"
     MODEL_DIR = "/opt/ml/model"
     WANDB_DIR = "/tmp/wandb"
 else:
@@ -26,19 +26,8 @@ else:
 
 BASELINE_OUTPUTS_DIR = os.path.join(OUTPUTS_DIR, "baseline")
 LORA_OUTPUTS_DIR = os.path.join(OUTPUTS_DIR, "lora")
-GRIDSEARCH_OUTPUTS_DIR = os.path.join(OUTPUTS_DIR, "grid_search")
 
-EXPERIMENTS_DIR = os.path.join(DATA_DIR, "experiments")
-OPENAI_FILES_DIR = os.path.join(EXPERIMENTS_DIR, "openai_files")
 
-# DATASET_FILE = os.path.join(ROOT_DIR, "dataset.jsonl")
-
-# CHECKPOINTS_DIR = os.path.join(ROOT_DIR, "checkpoints")
-
-# DEEP_SPEED_CONFIG_DIR = os.path.join(CODE_DIR, "lesson3", "deepspeed_config")
-
-# DEEP_SPEED_ZERO1_CONFIG = os.path.join(DEEP_SPEED_CONFIG_DIR, "zero1.json")
-
-# DEEP_SPEED_ZERO2_CONFIG = os.path.join(DEEP_SPEED_CONFIG_DIR, "zero2.json")
-
-# DEEP_SPEED_ZERO3_CONFIG = os.path.join(DEEP_SPEED_CONFIG_DIR, "zero3.json")
+# Bedrock-specific paths
+PROCESSED_DATA_DIR = os.path.join(DATA_DIR, "processed_data")
+BATCH_RESULTS_DIR = os.path.join(DATA_DIR, "batch_results")
